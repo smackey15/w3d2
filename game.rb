653bea129dd484034ee4e38f08207ed7e4d3b4c9
YWBK <ywbkdev@gmail.com>
2021-12-14 15:01:43 -0500

    7th swap

---
+++ game.rb
@@ -0,0 +1,32 @@
+class Game
+    def initialize(num)
+        @board = Board.new(num)
+        @prev_guess = #
+    end
+
+    def make_guess
+    end
+
+    def play
+        # render the board
+        # prompt the player for input
+        # get a guessed pos
+        # pass the guess pos to a make_guess method
+    end
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
